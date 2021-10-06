331354462a804e88e0e75a6b921f8d8948172363
Galambur <gaelle.derambure@epsi.fr>
2021-10-06 16:14:21 +0200

    réglé pb

--- ClientApp/components/home/home.ts
+++ ClientApp/components/home/home.ts
@@ -3,10 +3,11 @@ import { Component } from "vue-property-decorator";
 
 @Component
 export default class HomeComponent extends Vue {
-    public identifiant:string="";
-    public password:string="";
+    public identifiant: string = "";
+    public password: string = "";
     public verify_menu() {
         console.log(this.identifiant + this.password);
         
-        }
+        
     }
+}
