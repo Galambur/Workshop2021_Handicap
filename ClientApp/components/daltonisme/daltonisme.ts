69c5a21085465dded41caf7d982e8f8d6d22fb3d
Maxboo <mbourrier@booprod.com>
2021-10-06 15:50:29 +0200

    Daltonisme OK
    + justify sur les autres

--- ClientApp/components/daltonisme/daltonisme.ts
+++ ClientApp/components/daltonisme/daltonisme.ts
@@ -1,25 +1,17 @@
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 
-
 @Component
 export default class DaltonismeComponent extends Vue {
     
-    private _definition : number = 0;
-    public get definition() : number {
-        return this._definition;
-    }
-    public set definition(v : number) {
-        if(this._definition <= 1){
-            this._definition = v;
-        } else {
-            this._definition = 0;
-        }
-        
-    }
+    public definition : number = 1;
+    
 
     public changeDef(){
         this.definition += 1;
+        if(this.definition > 3){
+            this.definition = 0;
+        }
     }
 }
 
