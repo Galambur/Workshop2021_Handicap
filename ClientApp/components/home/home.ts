53e2a34e3f4f9272169926890bc515879f8c4ec7
MSI\mathias <mathias.boudou@gmail.com>
2021-10-06 12:38:07 +0200

    Fonction home pour cacher le menu

---
+++ ClientApp/components/home/home.ts
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class HomeComponent extends Vue {
+    public identifiant:string="";
+    public password:string="";
+    public verify_menu() {
+        console.log(this.identifiant + this.password);
+        if  (this.identifiant = "identifiant" && this.password = "password") {
+            
+        }
+
+        
+        }
+    }
